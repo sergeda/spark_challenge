5cb39defd9cf94bc43041a0c15b317ce836aece0
Serhii Dashko <sdashko@gmail.com>
2019-08-08 18:14:27 +0300

    Adds calculation using Dataset and custom Aggregator

--- src/main/scala/sergey/dashko/Event.scala
+++ src/main/scala/sergey/dashko/Event.scala
@@ -1,3 +1,4 @@
 package sergey.dashko
 
 case class Event (good: String, sum: Double, timestamp: Long, category: String, ip: String)
+case class EventDataShort(ip: String, sum: Double)
--- src/main/scala/sergey/dashko/spark/calculations/SumByCountryCalculations.scala
+++ src/main/scala/sergey/dashko/spark/calculations/SumByCountryCalculations.scala
@@ -2,12 +2,16 @@ package sergey.dashko.spark.calculations
 
 import cats.effect.IO
 import org.apache.commons.net.util.SubnetUtils
-import org.apache.spark.{Partitioner, RangePartitioner}
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Dataset, RelationalGroupedDataset, SparkSession}
-import sergey.dashko.{Event, SubnetInfo}
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.expressions.Aggregator
+import org.apache.spark.sql._
+import org.apache.spark.{Partitioner, RangePartitioner}
 import sergey.dashko.Main.timed
+import sergey.dashko.{Event, EventDataShort, SubnetInfo}
+
+import scala.collection.mutable.{Map => MMAP}
 import scala.reflect.ClassTag
 import scala.util.Try
 
@@ -20,9 +24,10 @@ class IpAddressPartitioner[K : Ordering : ClassTag, V](partitionsNumber: Int, rd
     if(string.length > 0) rangePartitioner.getPartition(string.substring(0,1)) else rangePartitioner.getPartition(string)
   }
 }
-class SumByCountryCalculations(dataset: Dataset[Event], spark: SparkSession, subnetData: RDD[String]) extends Serializable {
 
-  import spark.implicits._
+case class CombinedData(ip: String, sum: Double, network: String, countryId: Int)
+
+class SumByCountryCalculations(dataset: Dataset[Event], spark: SparkSession, subnetData: RDD[String]) extends Serializable {
 
   type Subnet = String
   type CountryId = Int
@@ -31,6 +36,12 @@ class SumByCountryCalculations(dataset: Dataset[Event], spark: SparkSession, sub
 
   val subnetDataParsed: RDD[(CountryId, Subnet)] = {
     subnetData.map(record => parseLine(record.split(","))).collect{case Some(v) => (v._1, v._2)}
+  }
+
+  val combOp: (MMAP[CountryId, Double], MMAP[CountryId, Double]) => MMAP[CountryId, Double] = (m1: MMAP[CountryId, Double], m2: MMAP[CountryId, Double]) => {
+    val elements1: List[(CountryId, Double)] = m1.toList
+    val elements2: List[(CountryId, Double)] = m2.toList
+    MMAP((elements1 ++ elements2).groupBy(_._1).mapValues(_.map{ case (_, v) => v}.sum).toSeq: _*)
   }
 
 
@@ -50,13 +61,8 @@ class SumByCountryCalculations(dataset: Dataset[Event], spark: SparkSession, sub
 
   //#2 Geo task solving using RDD and cartesian join - second quickest
   def sumByCountryRddCartesian(count: Int): IO[List[(CountryId, Double)]] = IO {
-    val addToMap = (map: Map[CountryId, Double], toAdd: Double, countryId: CountryId) => map.get(countryId).fold(map + (countryId -> toAdd))(currentValue => map + (countryId -> (currentValue + toAdd)))
-    val seqOp = (accum: Map[CountryId, Double], element: ((Subnet, CountryId), (String, Double))) => if(ipInSubnet(element._2._1, element._1._1)) addToMap(accum, element._2._2, element._1._2) else accum
-    val combOp = (m1: Map[CountryId, Double], m2: Map[CountryId, Double]) => {
-      val elements1: List[(CountryId, Double)] = m1.toList
-      val elements2: List[(CountryId, Double)] = m2.toList
-      (elements1 ++ elements2).groupBy(_._1).mapValues(_.map{ case (_, v) => v}.sum)
-    }
+    val addToMap = (map: MMAP[CountryId, Double], toAdd: Double, countryId: CountryId) => map.get(countryId).fold(map += (countryId -> toAdd))(currentValue => map += (countryId -> (currentValue + toAdd)))
+    val seqOp = (accum: MMAP[CountryId, Double], element: ((Subnet, CountryId), (String, Double))) => if(ipInSubnet(element._2._1, element._1._1)) addToMap(accum, element._2._2, element._1._2) else accum
 
     timed("sumByCountryRddCartesian",
       {
@@ -64,7 +70,7 @@ class SumByCountryCalculations(dataset: Dataset[Event], spark: SparkSession, sub
         val partitioner = new IpAddressPartitioner(flipped.getNumPartitions, flipped)
         val eventRdd = dataset.rdd.map{ event => (event.ip, event.sum)}.partitionBy(partitioner).persist()
         val partitionedEventRdd = flipped.partitionBy(partitioner).persist()
-        partitionedEventRdd.cartesian(eventRdd).aggregate(Map.empty[CountryId, Double])(seqOp, combOp)
+        partitionedEventRdd.cartesian(eventRdd).aggregate(MMAP.empty[CountryId, Double])(seqOp, combOp)
         .toList
         .sortBy(-_._2)
         .take(count)
@@ -97,6 +103,41 @@ class SumByCountryCalculations(dataset: Dataset[Event], spark: SparkSession, sub
         val df = spark.sql(s"SELECT subnets.countryId, SUM(events.sum) as sum FROM events join subnets ON true where ipInSubnet(events.ip, subnets.subnet) group by subnets.countryId order by sum desc limit ${count}")
             .as[(CountryId, Double)]
         df.collect().toList
+      })
+  }
+
+
+  //#5 Geo task solving using datasets crossJoin aggregator
+  def sumByCountryDatasetsCrossJoinAggregator(count: Int): IO[List[(CountryId, Double)]] = IO {
+    timed("sumByCountryDatasetsCrossJoinAggregator",
+      {
+        val eventData: Dataset[EventDataShort] = dataset.map(event => EventDataShort(event.ip, event.sum))
+        val subnetDataSet: Dataset[SubnetInfo] = subnetDataParsed.map(data => SubnetInfo(data._2, data._1)).toDS()
+        val combinedData: Dataset[CombinedData] = subnetDataSet.crossJoin(eventData).as[CombinedData]
+        val aggregator = new Aggregator[CombinedData, MMAP[CountryId, Double], List[(CountryId, Double)]] {
+
+          val mapEncoder: ExpressionEncoder[MMAP[CountryId, Double]] = ExpressionEncoder()
+          val listEncoder: ExpressionEncoder[List[(CountryId, Double)]] = ExpressionEncoder()
+
+          override def zero: MMAP[CountryId, Double] = MMAP.empty[CountryId, Double]
+
+          override def reduce(b: MMAP[CountryId, Double], a: CombinedData): MMAP[CountryId, Double] = {
+            if(ipInSubnet(a.ip, a.network)){
+              b.get(a.countryId).map( current => b += (a.countryId -> (current + a.sum)))
+                .getOrElse(b += (a.countryId -> a.sum))
+            } else b
+
+          }
+
+          override def merge(b1: MMAP[CountryId, Double], b2: MMAP[CountryId, Double]): MMAP[CountryId, Double] = combOp(b1, b2)
+
+          override def bufferEncoder: Encoder[MMAP[CountryId, Double]] = mapEncoder
+
+          override def finish(reduction: MMAP[CountryId, Double]): List[(CountryId, Double)] = reduction.toList.sortBy(- _._2)
+
+          override def outputEncoder: Encoder[List[(CountryId, Double)]] = listEncoder
+        }
+          combinedData.select(aggregator.toColumn).collect.toList.flatten.take(count)
       })
   }
 
